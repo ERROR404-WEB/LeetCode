542877b93fd874ef6aff8013046684883e78b1c1
ARJUN KUMAR <unabletofindmail@gmail.com>
2023-06-25 13:34:55 +0530

    Time: 49 ms (94.68%) | Memory: 42.1 MB (45.51%) - LeetSync

---
+++ apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let a=[]
+    for (let i =0;i<arr.length;i++)
+    {
+        a[i]=fn(arr[i],i);
+    }
+    return a;
+};
